4f9c1b2dccb311f33ee678c503b67a5b380c44b8
ayahmeftah <ayahmeftah123123@gmail.com>
2025-07-09 11:30:03 +0300

    grid is ready

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,25 @@
 function init(){
 
-    
+    // cached refrences
+    const gridElem = document.querySelector('.grid');
+
+    const cells = []
+    const gridWidth = 10
+    const numberOfCells = gridWidth*gridWidth
+
+    function createGrid(){
+        // for every cell that we require create a div 
+        // append this cell to our grid
+        for (let i = 0; i < numberOfCells; i++){
+            const cell = document.createElement('div');
+            cell.textContent = i;
+            cells.push(cell);
+            gridElem.appendChild(cell);
+
+        }
+        console.log(cells)
+    }
+
+    createGrid()
 }
 document.addEventListener('DOMContentLoaded',init);
