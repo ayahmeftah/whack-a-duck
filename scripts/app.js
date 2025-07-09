a2558abf250b2b81e9151f2cb6036c5f2b936ad2
ayahmeftah <ayahmeftah123123@gmail.com>
2025-07-09 13:32:45 +0300

    randomize and display duck position

--- scripts/app.js
+++ scripts/app.js
@@ -7,12 +7,30 @@ function init(){
     const gridWidth = 10
     const numberOfCells = gridWidth*gridWidth
 
+    let duckPosition = 58
+
+    function addDuck(){
+        cells[duckPosition].classList.add('duck')
+    }
+    
+    function removeDuck(){
+        cells[duckPosition].classList.remove('duck')
+    }
+    
+    function play(){
+        setInterval(() => {
+            removeDuck()
+            duckPosition = Math.floor(Math.random() * numberOfCells)
+            addDuck()
+        }, 3000);
+    }
+
     function createGrid(){
         // for every cell that we require create a div 
         // append this cell to our grid
         for (let i = 0; i < numberOfCells; i++){
             const cell = document.createElement('div');
-            cell.classList.add('duck');
+            // cell.classList.add('duck');
             cell.textContent = i;
             cells.push(cell);
             gridElem.appendChild(cell);
@@ -20,7 +38,9 @@ function init(){
         }
         console.log(cells)
     }
-
+    
     createGrid()
+    play()
 }
+
 document.addEventListener('DOMContentLoaded',init);
